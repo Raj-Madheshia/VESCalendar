59c3b0a52a17aecdccc9fcc96d5834d36a31af2d
karna98 <vedantwakalkar@gmail.com>
2019-10-12 17:13:27 +0530

    Improve Version of app

--- app/src/main/java/com/example/vescalendar/FragmentEvents.java
+++ app/src/main/java/com/example/vescalendar/FragmentEvents.java
@@ -1,44 +1,41 @@
 package com.example.vescalendar;
 
+import android.app.usage.UsageEvents;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.RecyclerView;
 
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class FragmentEvents extends Fragment {
-
-
-//    String titles[] = {"Event1","Event2", "Event3","Event4","Event5","Event6"};
-//    //String descriptions[] = {"Description 1", "Description 2","Description 3","Description 4","Description 5","Description 6"};
-//    String times[] = {"123","123","123","123","123","123"};
-//    String dates[] = {"123","123","123","123","123","123"};
-
+    CompactCalendarView compactCalendarView;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
     ListView listView;
-    private Toolbar toolbar;
+    TextView yearmonth;
+
     FloatingActionButton floatingActionButton;
 
     SqliteDatabaseHelper sqliteDatabaseHelper;
@@ -47,10 +44,20 @@ public class FragmentEvents extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
         getActivity().setTitle("My Events");
-        View view = inflater.inflate(R.layout.fragment_events, container, false);
+        final View view = inflater.inflate(R.layout.fragment_events, container, false);
+
+        compactCalendarView = (CompactCalendarView) view.findViewById(R.id.compactcalendar_view);
         listView = view.findViewById(R.id.listview);
-        listView.setClickable(true);
+        yearmonth = (TextView)view.findViewById(R.id.month);
+
+        compactCalendarView.setUseThreeLetterAbbreviation(true);
+        Event ev;
         sqliteDatabaseHelper = new SqliteDatabaseHelper(view.getContext());
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm");
+        Date gmt = null;
+
+
         String ID[];
         String TITLE[];
         String TIME[];
@@ -69,7 +76,23 @@ public class FragmentEvents extends Fragment {
                 TITLE[i] = res.getString(1);
                 DATE[i] = res.getString(3);
                 TIME[i] = res.getString(4);;
+                String date = DATE[i];
+                String title = TITLE[i];
+                String time = TIME[i];
+                String id = ID[i];
+
+                try {
+                    gmt = formatter.parse(date +'T'+time);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                long millisecondsSinceEpoch0 = gmt.getTime();
+                //String asString = formatter.format(gmt);
+
+                ev = new Event(Color.GREEN, millisecondsSinceEpoch0, id);
+                compactCalendarView.addEvent(ev);
                 i+=1;
+
             }
         }
         else{
@@ -80,44 +103,65 @@ public class FragmentEvents extends Fragment {
             TIME= new String[size];
         }
 
-        MyAdapter adapter = new MyAdapter(view.getContext(), ID,TITLE, TIME, DATE);
-        listView.setAdapter(adapter);
-
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String data=(String)adapterView.getItemAtPosition(position);
-                Log.w("Clicked", data );
-                Cursor res = sqliteDatabaseHelper.getValById(data);
-                String e_id="";
-                String e_title="";
-                String e_desp="";
-                String e_date="";
-                String e_time="";
-                while(res.moveToNext()) {
-                    e_id = res.getString(0);
-                    e_title = res.getString(1);
-                    e_desp = res.getString(2);
-                    e_date = res.getString(3);
-                    e_time = res.getString(4);
-                    Log.w("printing data", e_id + " " + e_title + " " + e_desp + " " + e_date + " " + e_time);
-                    //getFragmentManager().beginTransaction().replace(R.id.fragment_container, new DisplayEachMyEvents()).addToBackStack("FragementEvents").commit();
-
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            public void onDayClick(Date dateClicked) {
+                List<Event> events = compactCalendarView.getEvents(dateClicked);
+                String ID[];
+                String TITLE[];
+                String TIME[];
+                String DATE[];
+                int i =0;
+                int sizeoflist =0;
+                if(events.size()>0) {
+                    for (Event item : events) {
+                        String id = (String) item.getData();
+                        Cursor res = sqliteDatabaseHelper.getValById(id);
+                        sizeoflist+=res.getCount();
+                    }
                 }
-                Intent intent = new Intent(getActivity(), DisplayEachMyEvents.class);
-                Bundle dataBundle = new Bundle();
-                dataBundle.putString("id",e_id);
-                dataBundle.putString("title",e_title);
-                dataBundle.putString("desp",e_desp);
-                dataBundle.putString("date",e_date);
-                dataBundle.putString("time",e_time);
-                intent.putExtras(dataBundle);
-                startActivity(intent);
+                ID= new String[sizeoflist];
+                TITLE = new String[sizeoflist];
+                DATE = new String[sizeoflist];
+                TIME = new String[sizeoflist];
+
+                for (Event item : events) {
+                    String id = (String) item.getData();
+                    Cursor res = sqliteDatabaseHelper.getValById(id);
+                    if (res.getCount() != 0) {
+                        while (res.moveToNext()) {
+                            ID[i] = res.getString(0);
+                            TITLE[i] = res.getString(1);
+                            DATE[i] = res.getString(3);
+                            TIME[i] = res.getString(4);
+                            i += 1;
+                        }
+                    } else {
+                        int size = 0;
+                        ID = new String[size];
+                        TITLE = new String[size];
+                        DATE = new String[size];
+                        TIME = new String[size];
+                    }
+                }
+                MyAdapter adapter = new MyAdapter(view.getContext(), ID, TITLE, TIME, DATE);
+                listView.setAdapter(adapter);
 
             }
 
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                long full = firstDayOfNewMonth.getTime();
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm");
+                String asString = formatter.format(full);
+                String Year= asString.substring(0,4);
+                String month = asString.substring(5,7);
+                String months[] ={"Jan","Feb","Mar","Apr", "May", "Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+                int monthint = Integer.parseInt(month) -1;
+                yearmonth.setText(months[monthint]+" "+Year);
+                Log.d("Month", "Month was scrolled to: " + firstDayOfNewMonth);
+            }
         });
+
 
         floatingActionButton = (FloatingActionButton)view.findViewById(R.id.floatingActionButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -127,11 +171,9 @@ public class FragmentEvents extends Fragment {
             }
         });
 
-
         return view;
     }
-
-    class MyAdapter extends ArrayAdapter<String>{
+    class MyAdapter extends ArrayAdapter<String> {
         Context c;
         String mTitle[];
         //String mDesp[];
@@ -139,36 +181,39 @@ public class FragmentEvents extends Fragment {
         String mdate[];
         String Id[];
 
-        MyAdapter(Context c, String Id[],String title[], String time[], String date[]){
-            super(c, R.layout.eventlayout,R.id.eventTitle,Id);
+        MyAdapter(Context c, String Id[], String title[], String time[], String date[]) {
+            super(c, R.layout.eventlayout, R.id.eventTitle, Id);
             this.c = c;
             this.mTitle = title;
             //this.mDesp = descp;
             this.mdate = date;
             this.mtime = time;
             this.Id = Id;
-            //Log.d("To Test This", Arrays.toString(title)+" || "+Arrays.toString(descp)+"||"+Arrays.toString(date)+"||");
+
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater layoutInflater = (LayoutInflater)getActivity().getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View event = layoutInflater.inflate(R.layout.eventlayout,parent,false);
+
+            LayoutInflater layoutInflater = (LayoutInflater) getActivity().getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View event = layoutInflater.inflate(R.layout.eventlayout, parent, false);
 
             TextView eventTitle = event.findViewById(R.id.eventTitle);
-            // TextView eventDesp = event.findViewById(R.id.eventdescp);
             TextView eventDate = event.findViewById(R.id.eventdate);
             TextView eventTime = event.findViewById(R.id.eventtime);
 
 
             eventTitle.setText(mTitle[position]);
             //eventDesp.setText(mDesp[position]);
-            eventDate.setText(mdate[position]);
-            eventTime.setText(mtime[position]);
+            eventDate.setText("Date: "+mdate[position]);
+            eventTime.setText("Time: "+mtime[position]);
 
             return event;
         }
     }
 
 }
+
+
+
 
